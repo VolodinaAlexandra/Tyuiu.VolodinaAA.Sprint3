5880c9925d01c0add49630f2b7ac2bbd5564d483
tyuiu.volodinaaa@outlook.com <alexavolodina2006@gmail.com>
2023-10-24 13:45:06 +0300

    Task6

---
+++ Tyuiu.VolodinaAA.Sprint3.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VolodinaAA.Sprint3.Task6.V4.Lib;
+
+namespace Tyuiu.VolodinaAA.Sprint3.Task6.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetSumTheDivisorsValid()
+        {
+            DataService ds = new DataService();
+            int startValue = 14;
+            int stopValue = 20;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 8;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VolodinaAA.Sprint3.Task6.V4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VolodinaAA.Sprint3.Task6.V4.Lib;
+
+namespace Tyuiu.VolodinaAA.Sprint3.Task6.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт#3 |Выполнила: Володина А.А.|ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#3                                                                *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнила: Володина Александра Александровна | ПКТб-23-1                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [14, 20] количество всех делителей больше 9           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение начала отрезка");
+            int startValue;
+            startValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение конца отрезка");
+            int stopValue;
+            stopValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res;
+            res = ds.GetSumTheDivisors(startValue, stopValue);
+            Console.WriteLine($"Количество делителей отрезка [{startValue}, {stopValue}] равно " + res);
+            Console.ReadKey();
+        }
+    }
+}
