394885a466fd1fad1f9511cb88772dcc07bd56a7
tyuiu.volodinaaa@outlook.com <alexavolodina2006@gmail.com>
2023-10-24 14:51:17 +0300

    Task7

---
+++ Tyuiu.VolodinaAA.Sprint3.Task7.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VolodinaAA.Sprint3.Task7.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VolodinaAA.Sprint3.Task7.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be0b0c2a-135a-4c2a-bac5-43930d613026")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
